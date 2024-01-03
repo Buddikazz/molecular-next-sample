c04d913a52f6cb0d13019d9c203fee190a49c328
AshfaaqAhamed17 <ashfaaqahamed17@gmail.com>
2024-01-03 19:01:07 +0530

    Updated button component

--- light-modules/spa-next-app/brochure_next-app/components/atoms/Button.tsx
+++ light-modules/spa-next-app/brochure_next-app/components/atoms/Button.tsx
@@ -1,32 +1,34 @@
 export interface ButtonProps {
   styleClass?: string;
   name: string;
-  // get button type from magnolia and it will be used to set button style
   type?: string;
+  rounded?: boolean;
+  hoverColor?: string;
+  btnColor?: string;
+  textColor?: string;
 }
 
 export default function Button({
   styleClass = "",
   name = "",
   type = "primary",
+  rounded = false,
+  btnColor = "",
+  textColor = "",
+  hoverColor = "",
 }: ButtonProps) {
-  //   const buttonClasses = `${styleClass} w-40 flex items-center justify-center h-10 rounded-md ${
-  //     type === "outline"
-  //       ? "bg-white text-orange hover:text-white border border-orange"
-  //       : "bg-orange text-white"
-  //   } text-18 cursor-pointer hover:bg-dark-orange `;
-
-  let buttonClasses = `${styleClass} border border-solid rounded px-4 py-2 min-w-64 hover:bg-dark-orange`;
+  let buttonClasses = `${styleClass} border border-solid ${
+    rounded ? "rounded" : ""
+  } px-4 py-2 min-w-64`;
 
   if (type === "primary") {
-    buttonClasses += " bg-orange text-white";
+    buttonClasses += ` bg-${btnColor} text-${textColor} hover:bg-${hoverColor}`;
   } else if (type === "secondary") {
-    buttonClasses += " bg-white text-orange border-orange hover:text-white";
+    buttonClasses += ` bg-white text-${btnColor} border-${btnColor} hover:text-white hover:bg-${hoverColor}`;
   }
 
   return (
     <button className={buttonClasses}>
-      {" "}
       {type === "primary" && <i className="fas fa-caret-right mr-4"></i>}
       {name}
     </button>
